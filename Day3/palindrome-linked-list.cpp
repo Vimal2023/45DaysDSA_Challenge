fa366706996a13f8237e9d43909efcb1c28553f3
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-07-17 20:38:33 +0530

    Pro05

---
+++ Day3/palindrome-linked-list.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        if (fast != nullptr) {
+            slow = slow->next;
+        }
+        slow = reverseList(slow);
+        fast = head;
+
+        while (slow != nullptr) {
+            if (fast->val != slow->val)
+                return false;
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return true;
+    }
+
+private:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        while (head != nullptr) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+};
+
+int main() {
+    Solution sol;
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(2);
+    head->next->next->next = new ListNode(1);
+
+    if (sol.isPalindrome(head)) {
+        cout << "The linked list is a palindrome." << endl;
+    } else {
+        cout << "The linked list is not a palindrome." << endl;
+    }
+    while (head != nullptr) {
+        ListNode* next = head->next;
+        delete head;
+        head = next;
+    }
+
+    return 0;
+}
